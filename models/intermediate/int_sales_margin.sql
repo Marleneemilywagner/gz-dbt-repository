21d77550b8028e9c3f577b1c955d7a0e1ca7a37d
marlene.wagner1@googlemail.com <marlene.wagner1@googlemail.com>
2024-10-29 14:42:45 +0000

    margin and purchase cost

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,28 @@
+-- models/purchase_margin_calculation.sql
+
+WITH sales AS (
+    SELECT
+        orders_id,
+        products_id,
+        quantity,
+        revenue
+    FROM {{ ref('stg_gz_raw_data__raw_gz_sales') }}  -- Reference to the sales staging model
+),
+
+products AS (
+    SELECT
+        products_id,
+        purchase_price
+    FROM {{ ref('stg_gz_raw_data__raw_gz_product') }}  -- Reference to the product staging model
+)
+
+SELECT
+    s.orders_id,
+    s.products_id,
+    s.quantity,
+    s.revenue,
+    p.purchase_price,
+    (p.purchase_price * s.quantity) AS purchase_cost,  -- Calculate purchase cost
+    (s.revenue - (p.purchase_price * s.quantity)) AS margin  -- Calculate margin
+FROM sales s
+JOIN products p ON s.products_id = p.products_id 
